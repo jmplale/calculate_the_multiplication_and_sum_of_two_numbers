f34f9c78f822d0c2a0cb7854ad4463435c83dbde
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-21 20:14:22 +0800

    fixing slight error in if statement

--- exercise 1.py
+++ exercise 1.py
@@ -3,7 +3,8 @@ def solving(num_one, num_two):
     multiply = num_one * num_two
     addition = num_one + num_two
 
-    if multiply < 1000:
+    if multiply > 1000:
         return multiply
     else:
         return addition
+    
