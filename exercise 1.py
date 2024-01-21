8e8a17706993889ef755f0f453813ae0fd9c902f
johnmichaelpplale <johnmichaelpplale@gmail.com>
2024-01-21 20:16:44 +0800

    fixing errors in if-else statement

--- exercise 1.py
+++ exercise 1.py
@@ -4,7 +4,7 @@ def solving(num_one, num_two):
     addition = num_one + num_two
 
     if multiply > 1000:
-        return multiply
-    else:
         return addition
+    else:
+        return multiply
     
